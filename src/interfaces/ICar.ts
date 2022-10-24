f6c6b0b68060cbbf9a5cf87f82318aa2499888d5
Jeazi Ricardo <jeazi_ricardo@hotmail.com>
2022-10-24 18:40:52 -0300

    feat: cria a interface ICar

---
+++ src/interfaces/ICar.ts
@@ -0,0 +1,11 @@
+import { z } from 'zod';
+import { vehicleSchema } from './IVehicle';
+
+const carSchema = z.object({
+  doorsQty: z.number().int().gte(2).lte(4),
+  seatsQty: z.number().int().gte(2).lte(7),
+});
+
+const carMerge = carSchema.merge(vehicleSchema);
+
+export type ICar = z.infer<typeof carMerge>;
