f9f159419684cdd5cc9b93e0e19bf1bfabcac7d8
Jeazi Ricardo <jeazi_ricardo@hotmail.com>
2022-10-29 11:31:31 -0300

    feat: cria a rota POST para /cars

---
+++ src/routes/car.router.ts
@@ -0,0 +1,14 @@
+import { Router } from 'express';
+import CarController from '../controllers/car.controller';
+import CarModel from '../models/Car';
+import CarService from '../services/car.service';
+
+const carRouter = Router();
+
+const car = new CarModel();
+const carService = new CarService(car);
+const carController = new CarController(carService);
+
+carRouter.post('/', carController.create);
+
+export default carRouter;
