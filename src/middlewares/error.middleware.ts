f7d9ebc8ca3695dd5aa10e1ec06eeb8d0bc9a1fe
Jeazi Ricardo <jeazi_ricardo@hotmail.com>
2022-10-29 11:22:56 -0300

    feat: cria o middleware de error

---
+++ src/middlewares/error.middleware.ts
@@ -0,0 +1,17 @@
+import { NextFunction, Request, Response } from 'express';
+import { ZodError } from 'zod';
+import CustomError from '../errors/customError';
+
+export default function errorMiddleware(
+  err: CustomError | ZodError,
+  _req: Request,
+  res: Response,
+  _next: NextFunction,
+): Response {
+  if (err instanceof ZodError) { 
+    return res.status(400).json({ message: err.issues });
+  }
+  return res.status(err.status || 500).json({
+    message: err.message || 'Internal Server Error',
+  });
+}
