0b85f5219bd19bb4a14644b41ec734b17f91fb90
Jeazi Ricardo <jeazi_ricardo@hotmail.com>
2022-10-29 11:34:36 -0300

    feat: utiliza o middleware de error com o async-errors

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,13 @@
 import express from 'express';
+import 'express-async-errors';
+import errorMiddleware from './middlewares/error.middleware';
 import carRouter from './routes/car.router';
 
 const app = express();
 app.use(express.json());
 
 app.use('/cars', carRouter);
+
+app.use(errorMiddleware);
 
 export default app;
