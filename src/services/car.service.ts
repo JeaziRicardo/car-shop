75153e47784daf1f1cb76faf5c5fd320b7a301d3
Jeazi Ricardo <jeazi_ricardo@hotmail.com>
2022-10-29 11:27:11 -0300

    feat: cria a service de car implementando IService

---
+++ src/services/car.service.ts
@@ -0,0 +1,22 @@
+import { carMerge, ICar } from '../interfaces/ICar';
+import { IModel } from '../interfaces/IModel';
+import IService from '../interfaces/IService';
+
+export default class CarService implements IService<ICar> {
+  private _car: IModel<ICar>;
+
+  constructor(model: IModel<ICar>) {
+    this._car = model;
+  }
+
+  public create = async (car: ICar): Promise<ICar> => {
+    const parsed = carMerge.safeParse(car);
+
+    if (!parsed.success) {
+      throw parsed.error;
+    }
+    const result = await this._car.create(parsed.data);
+
+    return result;
+  };
+}
