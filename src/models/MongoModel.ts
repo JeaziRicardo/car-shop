50c364755962f4d88cdac22a6b3e73779195331c
Jeazi Ricardo <jeazi_ricardo@hotmail.com>
2022-10-29 11:08:57 -0300

    feat: implementa IModel em MongoModel

--- src/models/MongoModel.ts
+++ src/models/MongoModel.ts
@@ -1,9 +1,38 @@
-import { Model } from 'mongoose';
+import { Model, UpdateQuery } from 'mongoose';
+import { IModel } from '../interfaces/IModel';
 
-export default abstract class MongoModel<T> {
+export default abstract class MongoModel<T> implements IModel<T> {
   private _model: Model<T>;
 
   constructor(model: Model<T>) {
     this._model = model;
+  }
+
+  public async create(obj: T): Promise<T> {
+    const creates = await this._model.create(obj);
+    return creates;
+  }
+
+  public async read(): Promise<T[]> {
+    const reads = await this._model.find();
+    return reads;
+  }
+
+  public async readOne(_id: string): Promise<T | null> {
+    const read = await this._model.findOne({ _id });
+    return read;
+  }
+
+  public async update(_id: string, obj: T): Promise<T | null> {
+    const up = await this._model.findByIdAndUpdate(
+      { _id },
+      { ...obj } as UpdateQuery<T>,
+    );
+    return up;
+  }
+
+  public async delete(_id: string): Promise<T | null> {
+    const deletes = await this._model.findByIdAndDelete({ _id });
+    return deletes;
   }
 }
