c1673be9c4d9208fc7392ac445f25a7918923fbe
Jeazi Ricardo <jeazi_ricardo@hotmail.com>
2022-10-29 11:33:22 -0300

    feat: implementa as rotas de cars no src/app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,9 @@
 import express from 'express';
+import carRouter from './routes/car.router';
 
 const app = express();
+app.use(express.json());
+
+app.use('/cars', carRouter);
 
 export default app;
