a4de5ba04ecb98b9ed91b2406d8d671e81c1e9e1
Jeazi Ricardo <jeazi_ricardo@hotmail.com>
2022-10-29 15:45:15 -0300

    feat: cria um mock para os carros

---
+++ src/tests/mocks/car.mock.ts
@@ -0,0 +1,13 @@
+export const validCar = {
+  model: 'Uno da Escada',
+  year: 1963,
+  color: 'red',
+  buyValue: 3500,
+  seatsQty: 2,
+  doorsQty: 2
+};
+
+export const validCarId = {
+  _id: '5fee48c85931e0fb000gge03',
+  ...validCar,
+};
